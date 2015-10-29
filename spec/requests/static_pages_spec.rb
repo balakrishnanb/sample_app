2330966307018d267a6b281593c1b357fcd57d35
Balakrishnan Balasubramanian <balakrishnan.b@chronus.com>
2015-10-29 18:50:45 +0530

    setup css and layouts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,51 +4,51 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      visit home_path
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the right base title" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Sample App")
+      visit home_path
+      page.should have_selector('title', text: "Sample App")
     end
 
     it "should have the right custom title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => "| Home")
+      visit home_path
+      page.should_not have_selector('title', text: "| Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      visit help_path
+      page.should have_selector('h1', text: 'Help')
     end
     it "should have the right title" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => " | Help")
+      visit help_path
+      page.should have_selector('title', text: " | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      visit about_path
+      page.should have_selector('h1', text: 'About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => " | About")
+      visit about_path
+      page.should have_selector('title', text: " | About")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
     end
     it "should have the right title" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text => " | Contact")
+      visit contact_path
+      page.should have_selector('title', text: " | Contact")
     end
   end
 
