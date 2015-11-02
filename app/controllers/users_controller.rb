cb38bf4a9c151d99a2f98b22fc40b642928303d3
Balakrishnan Balasubramanian <balakrishnan.erode@gmail.com>
2015-11-02 08:20:56 +0530

    paginate with sample data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
       redirect_to new_user_registration_path
   end
   def index
-      @users = User.all
+      @users = User.paginate(page: params[:page])
   end
   def show
       @user = User.find(params[:id])
