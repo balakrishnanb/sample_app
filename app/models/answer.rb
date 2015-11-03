cf9e161f5c059ab8dd6b8b305a6ae03a4abc6204
Balakrishnan Balasubramanian <balakrishnan.b@chronus.com>
2015-11-03 16:44:05 +0530

    pull some code

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   attr_accessible :body
   validates :question_id, presence: true
+  validates :user_id, presence: true
   validates :body, presence: { message: "cant be blank" }, allow_blank: false
   validates :body, length: { minimum: 10 }, allow_blank: true
   default_scope order: 'answers.created_at ASC'
